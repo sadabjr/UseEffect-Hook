020fffee1dc9efbd9b3d33e5a03fed54a6d4bc9c
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2022-11-08 01:01:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseEffect from './component/UseEffect'
+
+const App = () => {
+  return (
+    <div className='App'>
+    <UseEffect/>
+    </div>
+  )
+}
+
+export default App
